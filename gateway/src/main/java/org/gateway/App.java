3fadde1de47971e6837ce0c42ee3d32f0798e4e8
sean <928946042@qq.com>
2017-04-13 23:38:00 +0800

    添加gateway子项目

---
+++ gateway/src/main/java/org/gateway/App.java
@@ -0,0 +1,15 @@
+package org.gateway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("hello ...");
+    }
+}
