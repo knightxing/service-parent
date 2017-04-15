5ae14d5ca0a328d2c6c6e966d4771b8f27c97851
sean <928946042@qq.com>
2017-04-15 12:15:36 +0800

    添加配置文件application.properties

--- gateway/src/main/java/org/gateway/controller/TestController.java
+++ gateway/src/main/java/org/gateway/controller/TestController.java
@@ -1,5 +1,36 @@
 package org.gateway.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
 public class TestController {
+
+	//@Autowired
+//	@Reference
+//	private TestService1 testService1;
+//
+//	//@Autowired
+//	@Reference
+//	private TestService2 testService2;
+
+	@RequestMapping("/hello/{content}")
+	@ResponseBody
+	public String hello(@PathVariable String content) {
+		Assert.notNull(content);
+		//String result = testService1.sayHello(content); // ִ
+		//return result;
+		return content;
+	}
+
+//	@RequestMapping("/showmoney")
+//	@ResponseBody
+//	public double showmoney() {
+//		return testService2.showMoney();
+//	}
 
 }
--- service1/src/main/java/org/service1/App.java
+++ service1/src/main/java/org/service1/App.java
@@ -1,13 +1,21 @@
 package org.service1;
 
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(App.class, args);
+
+	}
 }
